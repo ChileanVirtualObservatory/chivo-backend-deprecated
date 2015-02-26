03b1f4800abf9d9994f87e65c91dff7aa32ce3c6
Mauricio Araya <maray@inf.utfsm.cl>
2015-02-26 16:15:50 -0300

    JVO lurker

---
+++ lurker/jvolurker.sh
@@ -0,0 +1,4 @@
+wget "http://jvo.nao.ac.jp/portal/alma/archive.do"
+wget "http://jvo.nao.ac.jp/portal/alma/archive.do?action=project.info&projectCode=2011.0.00017.S"
+wget "http://jvo.nao.ac.jp/portal/alma/archive.do?action=dataset.info&datasetId=ALMA01002225"
+wget "http://jvo.nao.ac.jp/portal/alma/archive.do?action=download.fits&dataId=ALMA01002225_00_00_00"
